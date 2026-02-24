20d667885592fa1b4d31dc4c6c920c2abf1dd300
roman-yakubovskyi <yakubovskyi133@gmail.com>
2026-02-24 22:23:48 +0100

    add task -13

---
+++ src/task13.ts
@@ -0,0 +1,12 @@
+type Action = { type: "increment" | "decrement" };
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
